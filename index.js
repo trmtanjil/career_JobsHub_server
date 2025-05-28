8f4f0c93800a3fda09066a5d1917afbf4413707d
Tanjilur <trmtanjil02@gmail.com>
2025-05-28 15:47:55 +0600

    server side single data loaded commit

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const app = express()
 const cors =require('cors')
 const port =process.env.PORT || 3000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 //midle weare 
 app.use(cors())
 app.use(express.json())
-
 
 
 
@@ -22,13 +21,34 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+}
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const jobsCollection = client.db('careerCode').collection('jobs')
+
+
+
+        app.get('/jobs',async(req,res)=>{
+            const result = await jobsCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/jobs/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result =await jobsCollection.findOne(query);
+            res.send(result)
+        })
+
+
+
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
